f1016f7c9ed1292b194184c48e8792a3607b1339
TEO WEN ZHI <45276690+wenzhi0209@users.noreply.github.com>
2021-03-23 15:07:14 +0800

    update

--- Ass.py
+++ Ass.py
@@ -10,14 +10,12 @@ data = pd.read_csv('mbti.csv')
 
 # Text preprocessing steps - remove numbers, captial letters and punctuation
 def alphanumeric(x): return re.sub(r"""\w*\d\w*""", ' ', x)
+def punc_lower(x): return re.sub('[%s]' % re.escape(string.punctuation),' ', x.lower())
+def urlLink(x): return re.sub(r'(https|http)?:\/\/(\w|\.|\/|\?|\=|\&|\%)*\b',' ', x)
 
+data['posts'] = data.posts.map(urlLink).map(alphanumeric).map(punc_lower)
+data.head()
 
-def punc_lower(x): return re.sub(
-    '[%s]' % re.escape(string.punctuation), ' ', x.lower())
-
-
-data['posts'] = data.posts.map(alphanumeric).map(punc_lower)
-#data.head()
 
 # split the data into feature and label
 posts = data.posts  # inputs into model
@@ -27,15 +25,14 @@ X_train, X_test, y_train, y_test = train_test_split(
     posts, type, test_size=0.3, random_state=42)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-
 tfidf = TfidfVectorizer(stop_words='english')
 X_train = tfidf.fit_transform(X_train)
 X_test = tfidf.transform(X_test)
 
-from sklearn.linear_model import LogisticRegression
-model1 = LogisticRegression()
-model1.fit(X_train, y_train)
-model1.score(X_test, y_test)
+#from sklearn.linear_model import LogisticRegression
+#model1 = LogisticRegression()
+#model1.fit(X_train, y_train)
+#model1.score(X_test, y_test)
 
 from sklearn.neural_network import MLPClassifier
 mlp = MLPClassifier(solver='lbfgs', activation='logistic')
